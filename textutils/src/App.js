5d192aff29da1807d4f1249714ff79ea33aada2e
sandeshvora <sandeshvora96@gmail.com>
2023-06-28 16:55:04 +0530

    added about page

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+// import About from './components/About';
 
 function App() {
   return (
     <>
     <Navbar title="TextUtils" aboutText="About TextUtils" />
-    <div className="container my-3">
+    <div className="container my-3"> 
       <TextForm heading="Enter text to analyze" />
     </div>
-    
+    {/* <About /> */}
     </> 
   );
 }
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -9,15 +9,34 @@ export default function TextForm(props) {
         let newtext = text.toUpperCase();
         setText(newtext)
     }
+    const handleLoClick = ()=>{
+        let newtext = text.toLowerCase();
+        setText(newtext)
+    }
+    const handleclearText = ()=>{
+        let newtext = '';
+        setText(newtext)
+    }
     const [text,setText] = useState()
     return(
+        <>
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
         
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleclearText}>Clear Text</button>
     </div>
+    <div className="container my-3">
+        <h2> Your Text Summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
         )
 }
